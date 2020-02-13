ea7591d18c538dc8162b0cab2db5e7d5d126b410
Mikhail Rudenko <rudenko.michael@gmail.com>
2020-02-13 23:00:49 +0300

    Finish quick-sort JS

--- sorts/quick-sort.js
+++ sorts/quick-sort.js
@@ -22,4 +22,20 @@ function pivot(array, startIndex = 0, endIndex = array.length + 1) {
     return swapIndex;
 }
 
-pivot([7,3,4,1,3,5,1,6,7,3,4])
+// pivot([7,3,4,1,3,5,1,6,7,3,4])
+
+function quickSort(array, left = 0, right = array.length - 1) {
+    if (left < right) {
+        let pivotIndex = pivot(array, left, right);
+        //left
+        quickSort(array, left, pivotIndex - 1);
+        //right
+        quickSort(array, pivotIndex + 1, right)
+    }
+
+    return array;
+
+}
+
+
+quickSort([2,4,6,2,35,7,34,784,23,5,7]);
